38f8fdb3986168fd21549a587d0640ce38691bbd
Jeudi Prando <jeudiprando@gmail.com>
2022-04-29 02:36:12 -0300

    feat(ui): show lat lng in url

--- components/Mapa.vue
+++ components/Mapa.vue
@@ -1,44 +1,120 @@
 <template>
-    <KeepAlive>
-        <div :id="mapId" class="map" />
-    </KeepAlive>
+  <KeepAlive>
+    <div :id="mapId" class="map" />
+  </KeepAlive>
 </template>
 
 <script setup>
 import "leaflet/dist/leaflet.css";
 
 const props = defineProps({
-    markers: Array
-})
+  markers: Array,
+});
 
-const mapId = ref(`map_`)
+const mapId = ref(`map_`);
 
 if (process.client) {
-    let _map
-    onMounted(async () => {
-        mapId.value = `map_${Date.now()}`
-        const { map, tileLayer, marker } = await import("leaflet/src/Leaflet");
+  let _map;
+  const router = useRouter();
+  onMounted(async () => {
+    mapId.value = `map_${Date.now()}`;
 
-        _map = map(mapId.value).setView([51.505, -0.09], 13);
+    const { control, map, tileLayer, divIcon, marker } = await import(
+      "leaflet/src/Leaflet"
+    );
+    let markerCenter = null;
 
-        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(_map);
+    _map = map(mapId.value, {
+      zoomControl: false,
+      attributionControl: true,
+    }).setView([-20.361876858973783, -40.662597119808204], 17);
 
-        for (const { location, title } of props.markers) {
-            marker(location, { title })
-                .bindPopup(`<strong>${title}</strong><div>${location}<div>`)
-                .addTo(_map)
+    _map
+      // .on("click", () => {
+      // .on("movestart", () => {
+      .on("mousedown", () => {
+        if (markerCenter) {
+          markerCenter.remove();
         }
-    })
-    onUnmounted(() => {
-        _map.value.remove()
-    })
+        const { lat, lng } = _map.getCenter();
+        markerCenter = marker([lat, lng], {
+          icon: divIcon({
+            iconAnchor: [14, 16],
+            iconSize: [0, 0],
+            className: "makerCenter",
+            html: "➕",
+          }),
+        }).addTo(_map);
+      })
+      .on("move", () => {
+        if (_map && markerCenter) {
+          const { lat, lng } = _map.getCenter();
+          markerCenter.setLatLng([lat, lng]);
+        }
+      })
+      // .on("moveend", function (ev) {
+      .on("mouseup", function (ev) {
+        const { lat, lng } = _map.getCenter();
+        const zoom = _map.getZoom();
+        router.push(`/?lat=${lat}&lng=${lng}&z=${zoom}`);
+        if (markerCenter) {
+          markerCenter.remove();
+        }
+      });
+
+    control.scale().addTo(_map);
+    control
+      .zoom({
+        zoomOutTitle: "Diminuir zoom",
+        zoomInTitle: "Aumentar zoom",
+      })
+      .addTo(_map);
+
+    tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      // preferCanvas: true,
+      minZoom: 4,
+      maxZoom: 19,
+      updateInterval: 30000,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(_map);
+
+    for (const { location, title } of props.markers) {
+      marker(location, { title })
+        .bindPopup(`<strong>${title}</strong><div>${location}<div>`)
+        .addTo(_map);
+    }
+
+    _map
+      .locate({ enableHighAccuracy: true, watch: true, setView: true })
+      .on("locationfound", (ev) => {
+        marker(ev.latlng).addTo(_map);
+        const { latitude, longitude } = ev;
+        router.push(`/?lat=${latitude}&lng=${longitude}&z=17`);
+        _map.setZoom(17);
+      })
+      .on("locationerror", () => {
+        alert("initial location error");
+        _map.setView([-20.361876858973783, -40.662597119808204], 17);
+      });
+  });
+  onUnmounted(() => {
+    _map.value.remove();
+  });
 }
 </script>
 
 <style>
 .map {
-    height: 100vh
+  height: 100vh;
+}
+
+.makerCenter {
+  padding: 5px;
+  /* margin: auto; */
+  min-height: 18px;
+  min-width: 15px;
+  border: solid 4px #0005;
+  border-radius: 15px;
 }
 </style>
