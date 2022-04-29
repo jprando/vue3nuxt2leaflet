fb242f5264bf33295c8dd0b68485c913b209b0fa
Jeudi Prando <jeudiprando@gmail.com>
2022-04-29 03:25:38 -0300

    env(nuxt): config cdn url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
 export default defineNuxtConfig({
   app: {
-    baseURL: "/vue3nuxt3leaflet",
+    cdnURL: "https://jprando.github.io/vue3nuxt3leaflet",
   },
 });
